0e77f6bd1b5f4a8e830f9f86a7e5861d00077b13
ElormLokpo <benedictdev31@gmail.com>
2024-11-23 11:04:52 +0000

    REA-57 Backend models

---
+++ backend/src/entities/job-opening/job.opening.types.ts
@@ -0,0 +1,15 @@
+
+
+export enum JobTypeEnum{
+    PART_TIME = "PART_TIME",
+    FULL_TIME = "FULL_TIME",
+    CONTRACT = "CONTRACT",
+    INTERNSHIP = "INTERNSHIP",
+    OTHER = "OTHER"
+}
+
+export enum RemoteStateEnum{
+    REMOTE = "REMOTE",
+    IN_OFFICE = "IN_OFFICE",
+    BOTH = "BOTH"
+}
